ef710ea11a8dbce4c8d3c3419400f03ec90e2e10
Tabish Saleem <tabish.saleem@nxb.com.pk>
2017-07-26 15:06:01 +0500

    User: Completed user profile with bootstrap

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates_presence_of :first_name, :last_name, :mobile_phone, :about_me, :age, :city, :country, :facebook_name, :twitter_name, :skype_name
+  validates :mobile_phone, length: { is: 11 }
 end
